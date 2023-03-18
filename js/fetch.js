5b98823c9d2061ee70004fb46ebaca9417aa2f40
Nicklas <u2428296@rts-365.dk>
2023-03-18 17:24:07 +0100

    did everything the right way

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,13 @@
 const destinationlist = document.querySelector(".destinationlist")
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=400")
+fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then((response) => response.json())
     .then((data) => {
         data.results.forEach((pokemon) => {
             destinationlist.innerHTML += `
     <figure id="">
         <a href="detail.html?name=${pokemon.name}">${pokemon.name}</a>
-        <img src="${data.sprites.front_default}" alt="${pokemonData.name}">
+        
     </figure>
     `;
         });
